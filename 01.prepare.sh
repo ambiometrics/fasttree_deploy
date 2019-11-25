715fe24dd6310ea6cbb277653efd9cb23187281a
Edwin Rodríguez <edwrodrig@gmail.com>
2019-11-25 16:00:15 -0300

    windows branch

--- 01.prepare.sh
+++ 01.prepare.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 #This instructions are based on the README at following location http://www.microbesonline.org/fasttree/#Install
+#Can be done in linux
 
 #prepare directory
 rm -rf release
 mkdir release && cd release
 
 #download executable
-wget http://www.microbesonline.org/fasttree/FastTree
-
-#Change permission
-chmod +x FastTree
+wget http://www.microbesonline.org/fasttree/FastTree.exe
